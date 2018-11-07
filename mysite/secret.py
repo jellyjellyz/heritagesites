43018d47ddcd6e6c41df8f8cad6d7e39717655a7
Jiayu Zhang <jellyjellychris@gmail.com>
2018-11-07 17:10:28 -0500

    commit work

---
+++ mysite/secret.py
@@ -0,0 +1,4 @@
+SECRET_KEY = 'm-agegq-bhri5q1zrh)^%(5mx6znoe&!&h4=5o#0g%y1q1eg__'
+
+CLIENT_ID = '1024660924770-lol1ah9vum3ai18plaf0eci2l0j5a1r0.apps.googleusercontent.com'
+CLIENT_SECRET = '5p2ZOWZVkwCix9n7BSo0ZutD'
